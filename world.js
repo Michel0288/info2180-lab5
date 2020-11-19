24dd2196dcd567c0de4f1186ed639462804ff93d
Michel Davies <micheldavies0288@gmail.com>
2020-11-19 16:46:17 -0500

    Finished Ex. 5

--- world.js
+++ world.js
@@ -1,8 +1,16 @@
 onload= function(){
     var btn=document.getElementById('lookup');
+    var btn2=document.getElementById('lookup1');
     var httpRequest=new XMLHttpRequest();
     var result=document.getElementById('result');
     var country=document.getElementById('country');
+    btn2.addEventListener('click',function(event){
+        event.preventDefault();
+        var url="world.php?country="+country.value+"&context=cities";
+        httpRequest.onreadystatechange=hrequest;
+        httpRequest.open('GET',url,true);
+        httpRequest.send(); 
+    })
     btn.addEventListener('click',function(event){
         event.preventDefault();
         var url="world.php?country="+country.value;
